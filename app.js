492d8b1febc31b613dcc3542429979006539ed5c
mahmud009 <mahmud.est@gmail.com>
2023-05-13 22:30:43 +0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const container = document.getElementById("anim_container");
-const width = container.offsetHeight;
-const height = container.offsetWidth;
+const screen = document.getElementById("game-screen");
+const screenWidth = screen.offsetHeight;
+const screenHeight = screen.offsetWidth;
+
+function createElement({ name, child }) {
+  let element = document.createElement(name);
+  if (child != undefined) element.appendChild(child);
+  return element;
+}
 
 function randomPosition(count) {
   let coords = [];
@@ -11,15 +17,17 @@ function randomPosition(count) {
   return coords;
 }
 
-function createFoods() {
-  let coords = randomPosition(10);
+function createEnemies() {
+  let coords = randomPosition(2000);
   coords.forEach((coord) => {
-    console.log(coord);
-    let food = document.createElement("div");
+    let food = createElement({ name: "div" });
     food.classList.add("food");
     food.style.top = coord.y + "px";
     food.style.left = coord.x + "px";
-    container.append(food);
+    screen.append(food);
+    setInterval(() => {
+      food.style.left = food.offsetLeft + 1 + "px";
+    }, 100);
   });
 }
 
@@ -27,7 +35,7 @@ function createPlayer() {
   let player = document.createElement("div");
   player.classList.add("player");
   player.setAttribute("id", "player");
-  container.append(player);
+  screen.append(player);
 }
 
 function handleKeyboard() {
@@ -37,7 +45,7 @@ function handleKeyboard() {
     let player = document.getElementById("player");
     if (event.code == "ArrowRight") {
       console.log(player.offsetLeft);
-      console.log(player.offsetLeft >= width);
+      console.log(player.offsetLeft >= screenWidth);
       player.style.left = player.offsetLeft + speed + "px";
     }
     if (event.code == "ArrowLeft") {
@@ -53,5 +61,5 @@ function handleKeyboard() {
 }
 
 createPlayer();
-createFoods();
+createEnemies();
 handleKeyboard();
